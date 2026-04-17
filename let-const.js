70846b113e99156cd9d4b4f299aeb331fd6a1c97
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-04-17 11:50:46 +0600

    practiced let-const-var

--- let-const.js
+++ let-const.js
@@ -3,3 +3,30 @@ const name = 'Bangladesh';
 
 const country = 'BD_' + name;
 console.log(country);
+
+
+let price = 500;
+price = 400;
+console.log(price);
+
+const dress = ['pant', 'shirt', 'shoes'];
+// dress = ['pant','jama','shirt', 'shoes'];
+dress.push ('jama');
+console.log (dress);
+
+const person = {
+    name:'tisha',
+    age: 23,
+    height : 5.4,
+}
+
+// person = {
+//     name: 'tarin',
+//     age: 23,
+//     height: 5.4
+// }
+
+person.name = 'tarin';
+
+console.log (person);
+
