d53cbe7e1adbd1679996a03d0ce21737f46bacdd
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-04-19 22:00:49 +0600

    added rest.js file

---
+++ rest.js
@@ -0,0 +1,15 @@
+// spread operators
+
+const array = [1, 2, 3, 4];
+
+console.log(...array);
+
+// rest operators
+
+function numbers (...params) {
+   console.log(params);
+   
+}
+
+numbers (1,2,3,4);
+
