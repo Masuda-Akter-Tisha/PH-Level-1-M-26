4c08d26e59534c8d7c05ed3c789f380a1a841883
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-04-20 12:54:49 +0600

    added keys-value.js file

---
+++ keys-values.js
@@ -0,0 +1,21 @@
+const person = {
+    name : 'Masuda',
+    occupation : 'learner',
+    age : 23
+}
+
+// const name = person.name;
+// console.log(name);
+
+// key property
+const keys = Object.keys (person);
+console.log(keys);
+// value property
+const values = Object.values (person);
+console.log(values);
+
+// array of array
+const entries = Object.entries (person);
+console.log(entries);
+
+
